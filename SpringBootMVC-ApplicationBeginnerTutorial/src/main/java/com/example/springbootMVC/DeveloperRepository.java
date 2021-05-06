913848a4813cbe063653a62a4267ece482ae9ef7
ksvikas424 <srinivasvikask@gmail.com>
2021-05-06 19:12:41 +0530

    Spring Boot MVC Application Begineer Tutorial https://www.toptal.com/spring/beginners-guide-to-mvc-with-spring-framework

---
+++ SpringBootMVC-ApplicationBeginnerTutorial/src/main/java/com/example/springbootMVC/DeveloperRepository.java
@@ -0,0 +1,8 @@
+package com.example.springbootMVC;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DeveloperRepository extends CrudRepository<Developer, Long> {
+
+	
+}
